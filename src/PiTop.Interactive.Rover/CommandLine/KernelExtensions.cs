b5d0aee5500afe1a0fb35d7417732392b1b93a47
Diego Colombo <dicolomb@microsoft.com>
2021-05-18 18:18:00 +0100

    update dependencies

--- src/PiTop.Interactive.Rover/CommandLine/KernelExtensions.cs
+++ src/PiTop.Interactive.Rover/CommandLine/KernelExtensions.cs
@@ -20,14 +20,7 @@ namespace PiTop.Interactive.Rover.CommandLine
 {
     internal static class KernelExtensions
     {
-        public static T UseQuitCommand<T>(this T kernel, IDisposable disposeOnQuit, CancellationToken cancellationToken)
-            where T : Kernel
-        {
-            Quit.DisposeOnQuit = disposeOnQuit;
-            KernelCommandEnvelope.RegisterCommandType<Quit>(nameof(Quit));
-            cancellationToken.Register(async () => { await kernel.SendAsync(new Quit()); });
-            return kernel;
-        }
+
 
         public static T UseDefaultMagicCommands<T>(this T kernel)
     where T : Kernel
